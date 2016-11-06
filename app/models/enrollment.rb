5b2d9c12161ed1487fb4213cb976cf993977f592
Cathy Cullen <cathy@softwareoptions.com>
2016-11-06 13:59:17 -0600

    tweaked simple form stuff

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,8 +3,9 @@ class Enrollment < ActiveRecord::Base
   belongs_to :participant
   belongs_to :coordinator
   validates_presence_of :enrollment_date, :method_of_contact, :registry, :participant, :coordinator
-  
+
   validates_presence_of :remarks, :allow_blank => true
+  validates_uniqueness_of :registry_id, :scope => :participant_id
 
 
 end
